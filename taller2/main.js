dbb01137d0bd2768236ea41a406073ef5c5dbf13
danielamo98 <166255258+danielamo98@users.noreply.github.com>
2024-10-12 10:29:44 -0500

    Add files via upload

---
+++ taller2/main.js
@@ -0,0 +1,34 @@
+// Tienes un arreglo de números enteros. Utiliza los métodos funcionales .map(), .filter(), y .reduce() para realizar las siguientes operaciones:
+// Multiplica cada número por 2.
+// Filtra los números que sean mayores a 10.
+// Suma todos los números filtrados.
+
+// let numeros = [5,8,12,15,2,4,18,21];
+// let multiplicar = numeros.map(function(numeros){
+//     return numeros*2;
+// });
+// console.log(multiplicar);
+
+// let mayoresquediez = numeros.filter(function(numeros){
+//     return numeros >10;
+// });
+    
+// console.log(mayoresquediez);
+
+// let suma = numeros.filter(function(numeros){
+//     return numeros + numeros
+// });
+//     console.log(suma);
+
+let estudiante ={
+    nombre:'juan peresz',
+    edad: 20,
+    direccion:{
+        ciudad:'Medellin',
+        calle:'cll 10',
+        
+    }
+
+
+}
+
